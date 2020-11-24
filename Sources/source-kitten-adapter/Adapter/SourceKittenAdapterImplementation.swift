4a125ddbf7a60812af9fe8f02b9a45139985248b
incetro <incetro@ya.ru>
2020-11-24 12:42:25 +0400

    Fixed "key.parsed_declaration" structuring

--- Sources/source-kitten-adapter/Adapter/SourceKittenAdapterImplementation.swift
+++ Sources/source-kitten-adapter/Adapter/SourceKittenAdapterImplementation.swift
@@ -26,36 +26,51 @@ extension SourceKittenAdapterImplementation: SourceKittenAdapter {
     public func dictionary(forFileAt url: String) throws -> JSONDictionary {
 
         /// Clear environment from previous files if they are eixst
-        if let folder = Folder.current.subfolders.first(where: { $0.name == Constants.interlayerFolderName }) {
-            try folder.delete()
-        }
+        let workingFolder = Folder.current
+        let filesForCleaning = [
+            Constants.sourceKittenScriptName,
+            Constants.sourceKittenScriptOutputName
+        ]
+        try filesForCleaning
+            .filter {
+                workingFolder.containsFile(named: $0)
+            }
+            .map(File.init)
+            .forEach {
+                try $0.delete()
+            }
 
-        /// Copy target file to interlayer directory
-        let workingFolder = try Folder.current.createSubfolder(named: Constants.interlayerFolderName)
+        /// Copy target file to working directory
         let targetFile = try File(path: url)
-        try targetFile.copy(to: workingFolder)
+        if workingFolder.containsFile(named: targetFile.name) {
+            try workingFolder.file(named: targetFile.name).delete()
+        }
+        let workingFileCopy = try targetFile.copy(to: workingFolder)
 
         /// Do SourceKitten work
         let sourceKittenScript = Constants.sourceKittenScript(
-            inputFilePathString: targetFile.path,
-            outputFilePathString: workingFolder.path,
-            filename: targetFile.name
+            inputFilePathString: targetFile.name,
+            outputFilePathString: Constants.sourceKittenScriptOutputName
         )
-        try shellOut(to: sourceKittenScript)
+
+        let sourceKittenScriptFile = try workingFolder.createFile(named: Constants.sourceKittenScriptName)
+        try sourceKittenScriptFile.write("")
+        try sourceKittenScriptFile.append(sourceKittenScript)
+        try shellOut(to: "sh \(Constants.sourceKittenScriptName)")
+        try sourceKittenScriptFile.delete()
 
         /// Transform SourceKitten output to dictionary
-        let sourceKitContent = try workingFolder
-            .file(named: targetFile.name + "." + Constants.outputFileExtension)
-            .readAsString()
-        guard let data = sourceKitContent.data(using: .utf8) else {
+        let sourceKitContentFile = try workingFolder.file(named: Constants.sourceKittenScriptOutputName)
+        guard let data = try sourceKitContentFile.readAsString().data(using: .utf8) else {
             fatalError()
         }
         guard let json = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed) as? JSONDictionary else {
             fatalError()
         }
 
-        /// Clear interlayer folder
-        try workingFolder.delete()
+        /// Clear output files
+        try sourceKitContentFile.delete()
+        try workingFileCopy.delete()
 
         return json
     }
--- Sources/source-kitten-adapter/Common/Constants.swift
+++ Sources/source-kitten-adapter/Common/Constants.swift
@@ -11,15 +11,19 @@ import Foundation
 
 public enum Constants {
 
+    static let sourceKittenScriptName = "sk.sh"
     static let swiftExtension = "swift"
     static let interlayerFolderName = "incetro-working-directory"
     static let outputFileExtension = "txt"
 
+    static var sourceKittenScriptOutputName: String {
+        sourceKittenScriptName + "." + outputFileExtension
+    }
+
     static func sourceKittenScript(
         inputFilePathString: String,
-        outputFilePathString: String,
-        filename: String
+        outputFilePathString: String
     ) -> String {
-        "sourcekitten doc --single-file \(inputFilePathString) -- -j4 -sdk $(xcrun --show-sdk-path -sdk macosx) $(pwd)/\(inputFilePathString) > \(outputFilePathString)\(filename)" + "." + Constants.outputFileExtension
+        "sourcekitten doc --single-file \(inputFilePathString) -- -j4 -sdk $(xcrun --show-sdk-path -sdk macosx) $(pwd)/\(inputFilePathString) > \(outputFilePathString)"
     }
 }
